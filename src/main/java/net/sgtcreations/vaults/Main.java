275e133f2179e1b884db39f0af5dfe65151f7afe
BuildTools <unconfigured@null.spigotmc.org>
2023-06-02 23:12:41 +0100

    Initial commit

---
+++ src/main/java/net/sgtcreations/vaults/Main.java
@@ -0,0 +1,15 @@
+package net.sgtcreations.vaults;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static final String MOD_ID = "vaults";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sgtcreations/vaults/VaultsModClient.java
@@ -0,0 +1,10 @@
+package net.sgtcreations.vaults;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VaultsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+         
+    }
+}
